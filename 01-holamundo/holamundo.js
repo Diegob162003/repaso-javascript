d7c74c92601cfa7024d500014334735fbd28f1c7
Diego Buitrago <buitragodiego27@gmail.com>
2025-09-02 20:12:57 -0500

    repaso javascript

---
+++ 01-holamundo/holamundo.js
@@ -0,0 +1,27 @@
+console.log("Hola, Mundo!");
+
+let CajaAndy = "Hola, soy Andy";
+console.log(CajaAndy);
+
+let EdadAndy = 30;
+let NombreAndy = "Andy";
+console.log("Su edad es",EdadAndy ,"y su nombre es",NombreAndy);
+
+//arrays 
+
+let frutas = ["manzana", "banano", "mango"];
+console.log(frutas);
+console.log(frutas[1]);
+
+
+let frase = "hola, soy diego y me gusta programar";
+let mayusculas = "hola, soy diego y me gusta programar";
+let minusuculas = "hola, soy diego y me gusta programar";
+console.log(frase);
+console.log(frase.length);
+console.log(mayusculas.toUpperCase());
+console.log(minusuculas.toLowerCase());
+
+//operaciones matematicas
+const suma = 3+12;
+console.log(suma);
